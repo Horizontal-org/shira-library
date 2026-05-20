c87a002b135c008b11c5d9aa3dc922dbc958b2ab
fredzia <fredziaga@gmail.com>
2026-05-20 20:31:51 -0300

    feat: add docker compose configuration for DB and update database credentials in drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,5 +4,11 @@ export default defineConfig({
   schema: "./src/db/schema/*",
   out: "./src/db/migrations",
   dialect: "mysql",
-  dbCredentials: { url: process.env.DATABASE_URL! },
+  dbCredentials: {
+    host: process.env.MYSQL_HOST!,
+    port: Number(process.env.MYSQL_PORT) || 3306,
+    user: process.env.MYSQL_USER!,
+    password: process.env.MYSQL_PASSWORD!,
+    database: process.env.MYSQL_DATABASE!,
+  },
 });
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // remove
+  app.enableCors({
+    origin: [process.env.SPACE_URL],
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`shira-library running on port ${port}`);
