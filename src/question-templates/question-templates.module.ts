e1b26086df64628d1a22d788d858aa16e1440462
juandans01 <juandans01@gmail.com>
2026-06-02 15:09:10 -0300

    first draft question list

--- src/question-templates/question-templates.module.ts
+++ src/question-templates/question-templates.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common"
 import { QuestionTemplatesController } from "./controllers/question-templates.controller"
+import { ListQuestionsController } from "./controllers/list-questions.controller"
 import { QuestionTemplatesService } from "./services/question-templates.service"
+import { ListQuestionTemplatesService } from "./services/list-question-templates.service"
 import { DemoQuestionTemplatesController } from "./controllers/demo-question-templates.controller"
 import { DemoQuestionTemplatesService } from "./services/demo-question-templates.service"
 import { QuestionTemplatesCommand } from "./question-templates.command"
 
 @Module({
-  controllers: [QuestionTemplatesController, DemoQuestionTemplatesController],
-  providers: [QuestionTemplatesService, DemoQuestionTemplatesService, QuestionTemplatesCommand],
+  controllers: [QuestionTemplatesController, DemoQuestionTemplatesController, ListQuestionsController],
+  providers: [QuestionTemplatesService, DemoQuestionTemplatesService, QuestionTemplatesCommand, ListQuestionTemplatesService],
 })
 export class QuestionTemplatesModule {}
---
+++ src/question-templates/services/list-question-templates.service.ts
@@ -0,0 +1,123 @@
+import { Inject, Injectable } from "@nestjs/common"
+import { and, asc, count, desc, eq, inArray, like, SQL } from "drizzle-orm"
+import { MySql2Database } from "drizzle-orm/mysql2"
+import { DRIZZLE } from "../../db/drizzle.constants"
+import { questionTemplates } from "../../db/schema/question-templates"
+import { questionLangTags } from "../../db/schema/question-lang-tags"
+import { questionTags } from "../../db/schema/question-tags"
+import { langTags } from "../../db/schema/lang-tags"
+import { tags } from "../../db/schema/tags"
+import * as schema from "../../db/schema"
+import { ListQuestionTemplatesQuery } from "../dto/list-question-templates.dto"
+
+@Injectable()
+export class ListQuestionTemplatesService {
+  constructor(@Inject(DRIZZLE) private readonly db: MySql2Database<typeof schema>) { }
+
+  async findAll(query: ListQuestionTemplatesQuery) {
+    const conditions = this.buildConditions(query)
+    const where = conditions.length ? and(...conditions) : undefined
+    const { page, limit } = query
+
+    const [questions, [{ total }]] = await Promise.all([
+      this.db
+        .select()
+        .from(questionTemplates)
+        .where(where)
+        .orderBy(query.sortOrder === 'asc' ? asc(questionTemplates.createdAt) : desc(questionTemplates.createdAt))
+        .limit(limit)
+        .offset((page - 1) * limit),
+
+      this.db
+        .select({ total: count() })
+        .from(questionTemplates)
+        .where(where),
+    ])
+
+    if (questions.length === 0) return { data: [], total: Number(total), page, limit }
+
+    const questionIds = questions.map((q) => q.id)
+
+    const [langTagRows, tagRows] = await Promise.all([
+      this.db
+        .select({
+          questionId: questionLangTags.questionId,
+          id: langTags.id,
+          name: langTags.name,
+          code: langTags.code,
+        })
+        .from(questionLangTags)
+        .innerJoin(langTags, eq(questionLangTags.langTagId, langTags.id))
+        .where(inArray(questionLangTags.questionId, questionIds)),
+
+      this.db
+        .select({
+          questionId: questionTags.questionId,
+          id: tags.id,
+          name: tags.name,
+        })
+        .from(questionTags)
+        .innerJoin(tags, eq(questionTags.tagId, tags.id))
+        .where(inArray(questionTags.questionId, questionIds)),
+    ])
+
+    const data = questions.map((question) => ({
+      ...question,
+      langTags: langTagRows
+        .filter((r) => r.questionId === question.id)
+        .map(({ questionId: _, ...lt }) => lt),
+      tags: tagRows
+        .filter((r) => r.questionId === question.id)
+        .map(({ questionId: _, ...t }) => t),
+    }))
+
+    return { data, total: Number(total), page, limit }
+  }
+
+  private buildConditions(query: ListQuestionTemplatesQuery): SQL[] {
+    const conditions: SQL[] = []
+
+    if (query.search) {
+      conditions.push(like(questionTemplates.name, `%${query.search}%`))
+    }
+
+    if (query.filters.appType) {
+      conditions.push(eq(questionTemplates.appType, query.filters.appType))
+    }
+
+    if (query.filters.isPhishing !== undefined) {
+      conditions.push(eq(questionTemplates.isPhishing, query.filters.isPhishing))
+    }
+
+    if (query.filters.langTags?.length) {
+      const codes = query.filters.langTags
+      conditions.push(
+        inArray(
+          questionTemplates.id,
+          this.db
+            .select({ questionId: questionLangTags.questionId })
+            .from(questionLangTags)
+            .innerJoin(langTags, eq(questionLangTags.langTagId, langTags.id))
+            .where(inArray(langTags.code, codes))
+            .groupBy(questionLangTags.questionId),
+        ),
+      )
+    }
+
+    if (query.filters.tags?.length) {
+      const tagIds = query.filters.tags
+      conditions.push(
+        inArray(
+          questionTemplates.id,
+          this.db
+            .select({ questionId: questionTags.questionId })
+            .from(questionTags)
+            .where(inArray(questionTags.tagId, tagIds))
+            .groupBy(questionTags.questionId),
+        ),
+      )
+    }
+
+    return conditions
+  }
+}
