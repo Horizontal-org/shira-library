8b227111d810cbe7ba5d92b323099282d2d6c72c
juandans01 <juandans01@gmail.com>
2026-06-09 09:58:33 -0300

    add drizzle info to dockerfile and reenable cors

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,9 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { bufferLogs: true })
   app.useLogger(app.get(Logger))
 
-  //TEST ALL ORIGINS
-  // const origins = [process.env.SPACE_URL, process.env.SUPERADMIN_URL].filter((url): url is string => !!url)
+  const origins = [process.env.SPACE_URL, process.env.SUPERADMIN_URL].filter((url): url is string => !!url)
   app.enableCors({
-    origin: '*',
+    origin: origins,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Authorization', 'Content-Type', 'Accept'],
   });
